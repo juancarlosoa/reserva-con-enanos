9e9681427ec5c4bfc956c6957fb5bccbc5a7c37e
juancarlosoa <juankar.olivas@gmail.com>
2025-06-10 23:27:43 +0200

    feat: Enhance Vite configuration with improved server settings and watch options

--- RCE_HeroUI/vite.config.ts
+++ RCE_HeroUI/vite.config.ts
@@ -6,6 +6,18 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      host: 'localhost',
+      protocol: 'ws',
+      port: 5173
+    },
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
     proxy: {
       '/auth/api': {
         target: 'http://gateway:8080',
